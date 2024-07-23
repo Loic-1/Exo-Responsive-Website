5f19cee940434a756fd9079d401f90b9ff545b3a
Loic-1 <loic.vasile130@gmail.com>
2024-07-23 08:18:44 +0200

    ff

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,6 +51,37 @@ window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2500,
+    delay: 300,
+    // reset: true,
+})
+
+sr.reveal(`.home__img, .new__data, .care__img, .contact__content, .footer`)
+sr.reveal(`.home__img, .care__list, .contact__img`, {delay: 500})
+sr.reveal(`.home__img`, {delay: 500, interval: 100})
+sr.reveal(`.shop__img`, {interval: 100})
